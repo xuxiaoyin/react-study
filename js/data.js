939651f42a9f2ebd04410013da42fcdad8e16210
xuxiaoyin <279238850@qq.com>
2019-05-26 23:12:14 +0800

    QQlist

---
+++ js/data.js
@@ -0,0 +1,48 @@
+var dataList = {
+  friends: {
+    name: '好友',
+    list: [
+      {username: '张三', message: '下雨了', vip: true},
+      {username: '张四', message: '冒泡了', vip: true},
+      {username: '张无', message: '老头带着草帽了', vip: true},
+      {username: '张刘', message: '草帽尖', vip: false},
+      {username: '张其', message: '定制天', vip: true},
+      {username: '张吧', message: '草帽元', vip: true},
+      {username: '张就', message: '当雨伞', vip: true},
+      {username: '张是', message: '这这小船放里面', vip: false},
+      {username: '张十一', message: '一闪一闪靠岸边', vip: false},
+      {username: '张事儿', message: '下雨了', vip: true},
+      {username: '张十三', message: '下雨了', vip: true}
+    ]
+  },
+  families: {
+    name: '家人',
+    list: [
+      {username: '爸爸', message: '下雨了', vip: true},
+      {username: '妈妈', message: '冒泡了', vip: true},
+      {username: '姐姐', message: '老头带着草帽了', vip: true},
+      {username: '弟弟', message: '草帽尖', vip: false},
+      {username: '妹妹', message: '定制天', vip: true},
+      {username: '老公', message: '草帽元', vip: true},
+      {username: '儿子', message: '当雨伞', vip: true},
+      {username: '奶奶', message: '这这小船放里面', vip: false},
+      {username: '叔叔', message: '一闪一闪靠岸边', vip: false},
+      {username: '婶婶', message: '下雨了', vip: true},
+      {username: '姑姑', message: '下雨了', vip: true}
+    ]
+  },
+  colleague: {
+    name: '同事',
+    list: [
+      {username: '张工', message: '下雨了', vip: true},
+      {username: '理工', message: '冒泡了', vip: true},
+      {username: '武功', message: '老头带着草帽了', vip: true},
+      {username: '徐工', message: '草帽尖', vip: false},
+      {username: '刘工', message: '定制天', vip: true},
+      {username: '罗工', message: '草帽元', vip: true},
+      {username: '杜工', message: '当雨伞', vip: true},
+      {username: '王工', message: '这这小船放里面', vip: false},
+      {username: '刘工', message: '一闪一闪靠岸边', vip: false},
+    ]
+  },
+}
